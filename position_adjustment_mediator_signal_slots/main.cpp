62b255f0a49b346de75bf2a33b0e46b65fc36113
chicagohawk <voila@mit.edu>
2021-12-14 19:51:32 -0600

    comment

--- position_adjustment_mediator_signal_slots/main.cpp
+++ position_adjustment_mediator_signal_slots/main.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * Strategy::on_trade_event => (trade_signal) =>
+ * PositionMediator.on_trade_event => (eff_pos_signal) =>
+ * Strategy::on_eff_pos_event
+ */
+
 int main() {
     auto fgbs = Strategy {"FGBS"};
     auto fgbm = Strategy {"FGBM"};
